ba4d30806af8239cef0fede91b0e527e766554c1
Torgrim <lautor17@student.westerdals.no>
2018-11-10 16:24:56 +0100

    scanner og request

---
+++ pgr200-innlevering-starter-client/src/main/java/no/kristiania/pgr200/client/HttpRequest.java
@@ -0,0 +1,43 @@
+package no.kristiania.pgr200.client;
+
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class HttpRequest {
+    
+    
+    private String host;
+    private int port;
+
+    public HttpRequest(String host, int port) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        this.host = host;
+        this.port = port;
+        Socket clientSocket = new Socket(host, port);
+        
+        
+        clientSocket.getOutputStream().write((input + "\r\n").getBytes());
+        
+        clientSocket.close();
+        scanner.close();
+
+    }
+    
+    public static void main (String[] args) throws IOException {
+        System.out.println("Insert Command!");
+        new HttpRequest("localhost", 80);
+    }
+    
+    
+
+    
+    
+    
+    
+}
--- pgr200-innlevering-starter-server/src/main/java/no/kristiania/pgr200/database/HttpServer.java
+++ pgr200-innlevering-starter-server/src/main/java/no/kristiania/pgr200/database/HttpServer.java
@@ -5,10 +5,85 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.HashMap;
+import java.net.SocketImpl;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
 
-public class HttpServer{
-	
-	
+/*import no.kristiania.pgr200.core.ConferenceDao;
+import no.kristiania.pgr200.core.ConferenceTalk;
+import no.kristiania.pgr200.core.DatabaseMain;*/
+
+    public class HttpServer {
+    
+    private ServerSocket serverSocket;
+    private int port;
+   /* private ConferenceDao dao;
+    private DatabaseMain dataMain;*/
+    
+    
+    public HttpServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        start();
+    }    
+    
+    
+    public static void main (String[] args) throws IOException {
+        new HttpServer(80);
+        
+        }
+    
+    public void start() throws IOException {
+        new Thread(() -> {
+            try {
+                serverThread(serverSocket);
+            } catch (SQLException e) {
+                
+                e.printStackTrace();
+            }
+        }).start();
+    }
+    
+    public void serverThread(ServerSocket serverSocket) throws SQLException {
+        while (true) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                
+                InputStream input = clientSocket.getInputStream();
+                
+                String line = readNextLine(input);
+                while(!line.isEmpty()) {
+                    System.out.println(line);
+                    if (line.contains("list")) {
+                        String[] arguments = new String[] {"list"};
+                        //DatabaseMain.main(arguments);
+                    }
+                    line = readNextLine(input);
+                    
+                }
+                
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();    
+            }
+        }
+    }
+    
+    public static String readNextLine(InputStream input) throws IOException {
+        StringBuilder nextLine = new StringBuilder();
+        int c;
+        while ((c = input.read()) != -1) {
+            if (c == '\r') {
+                input.read();
+                break;
+            }
+            nextLine.append((char )c );
+        }
+        return nextLine.toString();
+        
+    }
+    
+    
+    
+    
 }
